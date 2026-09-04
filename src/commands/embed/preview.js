a4bc0a23027687524c49ff4a502d04738ec3d14e
zqcp <ivanrrr76@gmail.com>
2026-09-03 23:11:46 -0700

    Create preview.js

---
+++ src/commands/embed/preview.js
@@ -0,0 +1,86 @@
+const {
+    PermissionFlagsBits
+} = require("discord.js");
+
+const {
+    Embed
+} = require("../../models/Embed");
+
+const globalEmbeds =
+    require("../../embeds/general/global");
+
+const embed =
+    require("../../embeds/general/embed");
+
+module.exports = {
+
+    name: "embed preview",
+
+    async execute(client, message, args) {
+
+        // =========================
+        // USER PERMISSION
+        // =========================
+
+        if (
+            !globalEmbeds.permission(
+                message.member,
+                PermissionFlagsBits.ManageMessages
+            )
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.noPermission(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        // =========================
+        // EMBED NAME
+        // =========================
+
+        const name = args[0];
+
+        if (!name) {
+            return message.channel.send({
+                embeds: [
+                    embed.noName(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        // =========================
+        // FIND EMBED
+        // =========================
+
+        const savedEmbed =
+            await Embed.findOne({
+                guildId: message.guild.id,
+                name: name
+            });
+
+        if (!savedEmbed) {
+            return message.channel.send({
+                embeds: [
+                    embed.notFound(
+                        message.author,
+                        name
+                    )
+                ]
+            });
+        }
+
+        // =========================
+        // PREVIEW
+        // =========================
+
+        return message.channel.send({
+            content: savedEmbed.content || undefined,
+            embeds: savedEmbed.embeds || []
+        });
+    }
+};
