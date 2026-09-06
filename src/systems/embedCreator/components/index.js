aa83b2fe220dfb46ba5f9a2ddc8f73f2af0262a4
zqcp <ivanrrr76@gmail.com>
2026-09-06 09:45:02 -0700

    Create index.js

---
+++ src/systems/embedCreator/components/index.js
@@ -0,0 +1,29 @@
+const buttons = require("./buttons");
+const selects = require("./selects");
+const manager = require("./manager");
+
+// =========================
+// COMPONENT SYSTEM
+// =========================
+
+module.exports = {
+
+    // =========================
+    // BUTTONS
+    // =========================
+
+    buttons,
+
+    // =========================
+    // SELECT MENUS
+    // =========================
+
+    selects,
+
+    // =========================
+    // MANAGER
+    // =========================
+
+    manager
+
+};
