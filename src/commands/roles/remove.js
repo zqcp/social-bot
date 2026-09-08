bcdd12ece8cc61da99acc5869d7ab3fee389a943
zqcp <ivanrrr76@gmail.com>
2026-09-07 17:15:43 -0700

    Update remove.js

--- src/commands/roles/remove.js
+++ src/commands/roles/remove.js
@@ -2,11 +2,11 @@ const {
     PermissionFlagsBits
 } = require("discord.js");
 
-const globalEmbeds =
-    require("../../embeds/general/global");
-
 const roleEmbeds =
     require("../../embeds/general/roles");
+
+const globalEmbeds =
+    require("../../embeds/general/global");
 
 // =========================
 // COMMAND
@@ -14,9 +14,9 @@ const roleEmbeds =
 
 module.exports = {
 
-    name: "role remove",
+    name: "role add",
 
-    aliases: ["rr"],
+    aliases: ["r"],
 
     async execute(
         client,
@@ -105,27 +105,68 @@ module.exports = {
         // MEMBER
         // =========================
 
-        const member =
+        const memberValue =
+            args[0];
+
+        if (!memberValue) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.missing(
+                        message.author,
+                        "member"
+                    )
+                ]
+            });
+        }
+
+        let member =
             message.mentions.members.first();
+
+        if (!member) {
+
+            if (/^\d{17,20}$/.test(memberValue)) {
+
+                try {
+                    member =
+                        await message.guild.members.fetch(
+                            memberValue
+                        );
+                } catch {
+                    member = null;
+                }
+
+            } else {
+
+                member =
+                    message.guild.members.cache.find(
+                        member =>
+                            member.user.username.toLowerCase() ===
+                            memberValue.toLowerCase()
+                    );
+
+            }
+
+        }
 
         if (!member) {
             return message.channel.send({
                 embeds: [
-                    roleEmbeds.memberNotFound(
-                        message.author
+                    globalEmbeds.userNotFound(
+                        message.author,
+                        memberValue
                     )
                 ]
             });
         }
 
         // =========================
-        // ROLE
+        // ROLES
         // =========================
 
-        const role =
-            message.mentions.roles.first();
+        const roleValues =
+            args.slice(1);
 
-        if (!role) {
+        if (!roleValues.length) {
             return message.channel.send({
                 embeds: [
                     roleEmbeds.noRole(
@@ -133,6 +174,67 @@ module.exports = {
                     )
                 ]
             });
+        }
+
+        const roles = [];
+
+        for (const roleValue of roleValues) {
+
+            let role =
+                message.mentions.roles.find(
+                    role =>
+                        role.id === roleValue.replace(
+                            /[<@&>]/g,
+                            ""
+                        )
+                );
+
+            if (!role) {
+
+                if (/^\d{17,20}$/.test(roleValue)) {
+
+                    try {
+                        role =
+                            await message.guild.roles.fetch(
+                                roleValue
+                            );
+                    } catch {
+                        role = null;
+                    }
+
+                } else {
+
+                    role =
+                        message.guild.roles.cache.find(
+                            role =>
+                                role.name.toLowerCase() ===
+                                roleValue.toLowerCase()
+                        );
+
+                }
+
+            }
+
+            if (!role) {
+                return message.channel.send({
+                    embeds: [
+                        roleEmbeds.roleNotFound(
+                            message.author,
+                            roleValue
+                        )
+                    ]
+                });
+            }
+
+            if (
+                !roles.some(
+                    existingRole =>
+                        existingRole.id === role.id
+                )
+            ) {
+                roles.push(role);
+            }
+
         }
 
         // =========================
@@ -160,69 +262,77 @@ module.exports = {
         }
 
         // =========================
-        // USER HIERARCHY
+        // CHECK ROLES
         // =========================
 
-        if (
-            role.position >=
-            message.member.roles.highest.position
-        ) {
-            return message.channel.send({
-                embeds: [
-                    roleEmbeds.userRole(
-                        message.author,
-                        role
-                    )
-                ]
-            });
+        for (const role of roles) {
+
+            // USER HIERARCHY
+            if (
+                role.position >=
+                message.member.roles.highest.position
+            ) {
+                return message.channel.send({
+                    embeds: [
+                        roleEmbeds.userRole(
+                            message.author,
+                            role
+                        )
+                    ]
+                });
+            }
+
+            // BOT HIERARCHY
+            if (
+                role.position >=
+                botMember.roles.highest.position
+            ) {
+                return message.channel.send({
+                    embeds: [
+                        roleEmbeds.botRole(
+                            message.author,
+                            role
+                        )
+                    ]
+                });
+
+            }
+
+            // ALREADY HAS ROLE
+            if (
+                member.roles.cache.has(
+                    role.id
+                )
+            ) {
+                return message.channel.send({
+                    embeds: [
+                        roleEmbeds.alreadyHas(
+                            message.author,
+                            role
+                        )
+                    ]
+                });
+            }
+
         }
 
         // =========================
-        // BOT HIERARCHY
-        // =========================
-
-        if (
-            role.position >=
-            botMember.roles.highest.position
-        ) {
-            return message.channel.send({
-                embeds: [
-                    roleEmbeds.botRole(
-                        message.author,
-                        role
-                    )
-                ]
-            });
-        }
-
-        // =========================
-        // DOESN'T HAVE ROLE
-        // =========================
-
-        if (!member.roles.cache.has(role.id)) {
-            return message.channel.send({
-                embeds: [
-                    roleEmbeds.notHas(
-                        message.author,
-                        role
-                    )
-                ]
-            });
-        }
-
-        // =========================
-        // REMOVE ROLE
+        // ADD ROLES
         // =========================
 
         try {
 
-            await member.roles.remove(role);
+            await member.roles.add(
+                roles
+            );
 
             return message.channel.send({
                 embeds: [
-                    roleEmbeds.removeSuccess(
+                    roleEmbeds.success(
                         message.author,
-                        role,
+                        roles
+                            .map(role => role.toString())
+                            .join(", "),
                         member
                     )
                 ]
@@ -231,15 +341,17 @@ module.exports = {
         } catch (error) {
 
             console.error(
-                "Role Remove Error:",
+                "Role Add Error:",
                 error
             );
 
             return message.channel.send({
                 embeds: [
-                    roleEmbeds.removeFailed(
+                    roleEmbeds.failed(
                         message.author,
-                        role,
+                        roles
+                            .map(role => role.toString())
+                            .join(", "),
                         member
                     )
                 ]
