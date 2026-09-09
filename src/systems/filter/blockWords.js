2cb519ae4bc3ba2170ef02843b1e3340197a4b69
zqcp <ivanrrr76@gmail.com>
2026-09-09 12:54:01 -0700

    Create blockWords.js

---
+++ src/systems/filter/blockWords.js
@@ -0,0 +1,52 @@
+module.exports = [
+
+    // =========================
+    // SEVERE PROFANITY
+    // =========================
+
+    "fuck",
+    "fucking",
+    "fucker",
+    "motherfucker",
+    "shit",
+    "shitting",
+    "bullshit",
+    "bitch",
+    "bitches",
+    "asshole",
+    "dumbass",
+    "dickhead",
+    "cocksucker",
+    "pussy",
+    "cunt",
+    "dick",
+
+    // =========================
+    // SEXUAL
+    // =========================
+
+    "porn",
+    "pornography",
+    "hentai",
+    "nudes",
+    "blowjob",
+    "handjob",
+    "cum",
+    "jizz",
+
+    // =========================
+    // HARASSMENT
+    // =========================
+
+    "kys",
+    "kill yourself",
+    "go die",
+
+    // =========================
+    // HATE / SLUR PLACEHOLDERS
+    // =========================
+
+    "faggot",
+    "<fag",
+
+];
