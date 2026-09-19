d4b45ef6b7b09de02cb5e7094f3d8037e36e4b87
zqcp <ivanrrr76@gmail.com>
2026-09-19 02:28:17 -0700

    Create unjail.js

---
+++ src/commands/jail/unjail.js
@@ -0,0 +1,316 @@
+const {
+    PermissionFlagsBits
+} = require("discord.js");
+
+const Jail =
+    require("../../models/Jail");
+
+const globalEmbeds =
+    require("../../embeds/general/global");
+
+const jailEmbeds =
+    require("../../embeds/general/jail");
+
+const jailHelp =
+    require("../../embeds/help/jail");
+
+module.exports = {
+    name: "unjail",
+    aliases: ["unj"],
+    permissions: [
+        PermissionFlagsBits.ManageGuild
+    ],
+
+    async execute(client, message, args) {
+        if (!message.guild) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.error(
+                        "This command can only be used in a server."
+                    )
+                ]
+            });
+        }
+
+        if (
+            !message.member.permissions.has(
+                PermissionFlagsBits.ManageGuild
+            )
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.permission(
+                        message.author,
+                        "ManageGuild"
+                    )
+                ]
+            });
+        }
+
+        const botMember =
+            message.guild.members.me;
+
+        if (
+            !botMember.permissions.has(
+                PermissionFlagsBits.ManageRoles
+            )
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.botPermission(
+                        message.author,
+                        "ManageRoles"
+                    )
+                ]
+            });
+        }
+
+        const requiredPermissions = [
+            PermissionFlagsBits.ViewChannel,
+            PermissionFlagsBits.SendMessages,
+            PermissionFlagsBits.EmbedLinks
+        ];
+
+        const missingPermissions =
+            requiredPermissions.filter(
+                permission =>
+                    !message.channel
+                        .permissionsFor(botMember)
+                        ?.has(permission)
+            );
+
+        if (missingPermissions.length) {
+            const permissionNames =
+                missingPermissions.map(
+                    permission =>
+                        Object.entries(
+                            PermissionFlagsBits
+                        ).find(
+                            ([, value]) =>
+                                value === permission
+                        )?.[0] || permission
+                );
+
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.botPermissions(
+                        message.author,
+                        permissionNames
+                    )
+                ]
+            });
+        }
+
+        const target =
+            args[0];
+
+        if (!target) {
+            return message.channel.send({
+                embeds: [
+                    jailHelp.unjail(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        let member =
+            message.mentions.members.first();
+
+        if (!member) {
+            member =
+                await message.guild.members
+                    .fetch(target)
+                    .catch(() => null);
+        }
+
+        if (!member) {
+            member =
+                message.guild.members.cache.find(
+                    member =>
+                        member.user.username.toLowerCase() ===
+                        target.toLowerCase()
+                );
+        }
+
+        if (!member) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.userNotFound(
+                        message.author,
+                        target
+                    )
+                ]
+            });
+        }
+
+        if (member.id === message.author.id) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.self(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        if (member.id === message.guild.ownerId) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.owner(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        if (
+            member.roles.highest.position >=
+            message.member.roles.highest.position
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.hierarchy(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        if (
+            member.roles.highest.position >=
+            botMember.roles.highest.position
+        ) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.botRole(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        const jail =
+            await Jail.findOne({
+                guildId:
+                    message.guild.id
+            });
+
+        if (!jail) {
+            return message.channel.send({
+                embeds: [
+                    jailEmbeds.notSetup(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        const jailRole =
+            message.guild.roles.cache.get(
+                jail.roleId
+            );
+
+        if (!jailRole) {
+            return message.channel.send({
+                embeds: [
+                    jailEmbeds.notSetup(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        if (!jailRole.editable) {
+            return message.channel.send({
+                embeds: [
+                    globalEmbeds.botRole(
+                        message.author
+                    )
+                ]
+            });
+        }
+
+        const recordIndex =
+            jail.members.findIndex(
+                record =>
+                    record.userId ===
+                    member.id
+            );
+
+        if (recordIndex === -1) {
+            return message.channel.send({
+                embeds: [
+                    jailEmbeds.alreadyUnjailed(
+                        message.author,
+                        member
+                    )
+                ]
+            });
+        }
+
+        const record =
+            jail.members[recordIndex];
+
+        const rolesToRestore =
+            Array.isArray(record.roles)
+                ? record.roles
+                    .map(
+                        roleId =>
+                            message.guild.roles.cache.get(
+                                roleId
+                            )
+                    )
+                    .filter(
+                        role =>
+                            role &&
+                            role.id !==
+                                message.guild.id &&
+                            role.id !==
+                                jailRole.id &&
+                            role.editable
+                    )
+                    .map(
+                        role =>
+                            role.id
+                    )
+                : [];
+
+        try {
+            await member.roles.set(
+                rolesToRestore,
+                "Unjailed"
+            );
+
+            jail.members.splice(
+                recordIndex,
+                1
+            );
+
+            await jail.save();
+
+            return message.channel.send({
+                embeds: [
+                    jailEmbeds.unjailed(
+                        message.author,
+                        member
+                    )
+                ]
+            });
+        } catch (error) {
+            console.error(
+                "[UNJAIL] Failed to unjail member:",
+                error
+            );
+
+            return message.channel.send({
+                embeds: [
+                    jailEmbeds.unjailFailed(
+                        message.author,
+                        member
+                    )
+                ]
+            });
+        }
+    }
+};
