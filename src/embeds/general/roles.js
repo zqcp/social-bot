135aebefb4ac1396c5b09a09ab834774137b44dc
zqcp <ivanrrr76@gmail.com>
2026-09-20 18:37:54 -0700

    Update roles.js

--- src/embeds/general/roles.js
+++ src/embeds/general/roles.js
@@ -184,7 +184,7 @@ module.exports = {
         return new EmbedBuilder()
             .setColor(config.colors.role)
             .setDescription(
-                `${config.emojis.add} ${user}: Added ${roleName} to **${member.user.username}**.`
+                `${config.emojis.add} ${user}: Added \`${roleName}\` to **${member.user.username}**.`
             );
     },
 
@@ -202,7 +202,7 @@ module.exports = {
         return new EmbedBuilder()
             .setColor(config.colors.failed)
             .setDescription(
-                `${config.emojis.failed} ${user}: Failed to add ${roleName} to **${member.user.username}**. Please try again.`
+                `${config.emojis.failed} ${user}: Failed to add \`${roleName}\` to **${member.user.username}**. Please try again.`
             );
     },
 
@@ -225,7 +225,7 @@ module.exports = {
         return new EmbedBuilder()
             .setColor(config.colors.role)
             .setDescription(
-                `${config.emojis.remove} ${user}: Removed ${roleName} from **${member.user.username}**.`
+                `${config.emojis.remove} ${user}: Removed \`${roleName}\` from **${member.user.username}**.`
             );
     },
 
@@ -243,7 +243,7 @@ module.exports = {
         return new EmbedBuilder()
             .setColor(config.colors.failed)
             .setDescription(
-                `${config.emojis.failed} ${user}: Failed to remove ${roleName} from **${member.user.username}**. Please try again.`
+                `${config.emojis.failed} ${user}: Failed to remove \`${roleName}\` from **${member.user.username}**. Please try again.`
             );
     },
 
